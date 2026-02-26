0ee0241e3a492e986be0df0ba3d6e4eb41733d6d
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2026-02-26 17:28:58 +0600

    Update flutterwave-for-fluent-cart.php

--- flutterwave-for-fluent-cart.php
+++ flutterwave-for-fluent-cart.php
@@ -87,7 +87,7 @@ add_action('plugins_loaded', function() {
     /**
      * Plugin Updater
      */
-    $apiUrl = 'https://fluentcart.com/wp-admin/admin-ajax.php?action=fluent_cart_flutterwave_update&time=' . time();
+    $apiUrl = 'https://api.fluentcart.com/wp-admin/admin-ajax.php?action=fluent_cart_flutterwave_update&time=' . time();
     new \FlutterwaveFluentCart\PluginManager\Updater($apiUrl, FLUTTERWAVE_FCT_PLUGIN_FILE, array(
         'version'   => FLUTTERWAVE_FCT_VERSION,
         'license'   => '12345',
